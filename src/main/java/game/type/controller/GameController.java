da7932e534eca972006cccaa55ea3e989b504aa8
DARSHAN GOLCHHA <darshan@DARSHANs-MacBook-Air.local>
2023-11-05 09:40:42 -0600

    Adding stateless Controller

--- src/main/java/game/type/controller/GameController.java
+++ src/main/java/game/type/controller/GameController.java
@@ -1,78 +1,73 @@
 package game.type.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import game.type.model.Prey;
 import game.type.service.DistanceCalculator;
+import game.type.service.GameService;
 import game.type.service.GameState;
 import game.type.service.PlayerAnalyticsGen;
 import game.type.service.TextGen;
 
 @RestController
 public class GameController {
-	private Prey player;
-	private GameState gameState;
-	private static final int initialPosition= 10;
-	private static final int beginner= 1;
-	private TextGen text;
-	private String paragraph;
+	
+	@Autowired
+    private GameService gameService;
 	
 	
 	@PostMapping("/initialize")
     public ResponseEntity<String> initializeGame(@RequestParam String username) {
         // Initialize the player with the provided details
-        this.player = new Prey(username);
-        this.gameState = new GameState(initialPosition, player.getDistance(), beginner);
-        this.text = new TextGen();
-        paragraph = text.generateRandomParagraph();
-        return ResponseEntity.ok("Game initialized for " + username);
+		String sessionId = gameService.createGameSession(username);
+        return ResponseEntity.ok(sessionId);
     }
 
     @GetMapping("/getState")
-    public ResponseEntity<GameState> getGameState() {
-        // Return the current game state, including player details
-        if (player != null) {
-            return ResponseEntity.ok(this.gameState);
+    public ResponseEntity<GameState> getGameState(@RequestParam String sessionId) {
+    	// Retrieve the game state for the user identified by sessionId
+        GameState gameState = gameService.getGameState(sessionId);
+        if (gameState != null) {
+            return ResponseEntity.ok(gameState);
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
     }
     
     @GetMapping("/updateState")
-    public ResponseEntity<GameState> setGameState
-    (@RequestParam int timeElapsed, @RequestParam int correct, int wrong) {
-    	// Return the current game state, including player details
-        if (player != null) {
-        	int distance = DistanceCalculator.calculateDistance(timeElapsed, correct, wrong, player.getDistance(), player);
-        	player.setDistance(distance);
-        	this.gameState.updateState(player);
-        	paragraph = text.generateRandomParagraph();
-            return ResponseEntity.ok(this.gameState);
+    public ResponseEntity<GameState> updateGameState(@RequestParam String sessionId, @RequestParam int timeElapsed, @RequestParam int correct, @RequestParam int wrong) {
+        // Update the game state for the user identified by sessionId
+        GameState gameState = gameService.updateGameState(sessionId, timeElapsed, correct, wrong);
+        if (gameState != null) {
+            return ResponseEntity.ok(gameState);
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
     }
     
     @GetMapping("/userAnalysis")
-    public ResponseEntity<PlayerAnalyticsGen> getUserAnalysis(@RequestParam int timeElapsed, @RequestParam int correct, int wrong){
-    	if (player !=null) {
-    		return ResponseEntity.ok(new PlayerAnalyticsGen(timeElapsed, correct, wrong));
-    	}
-    	else {
-    		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-    	}
+    public ResponseEntity<PlayerAnalyticsGen> getUserAnalysis(@RequestParam String sessionId, @RequestParam int timeElapsed, @RequestParam int correct, @RequestParam int wrong) {
+        // Get user analytics for the user identified by sessionId
+        PlayerAnalyticsGen analytics = gameService.calculateUserAnalytics(sessionId, timeElapsed, correct, wrong);
+        if (analytics != null) {
+            return ResponseEntity.ok(analytics);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
     
     @GetMapping("/getText")
-    public ResponseEntity<String> randomTextGen(){
-    	if (this.paragraph != null) {
-    		return ResponseEntity.ok(paragraph);
-    	}
-    	else {
-    		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-    	}
+    public ResponseEntity<String> getRandomText(@RequestParam String sessionId) {
+        // Retrieve random text for the user identified by sessionId
+        String paragraph = gameService.getRandomText(sessionId);
+        if (paragraph != null) {
+            return ResponseEntity.ok(paragraph);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
     
     
